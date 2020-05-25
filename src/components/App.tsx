568dc00141ebdbf4a7a4c761558f9443f91143c0
Ravik Ganguly <theravikganguly@gmail.com>
2020-05-25 14:13:19 +0530

    feat: added react-router setup & basic Dashboard component

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,12 +1,47 @@
 import "bootstrap/dist/css/bootstrap.min.css";
 import React from "react";
-import Landing from "./Landing";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import { Loader } from "./widgets";
+
+/**
+ * Types
+ */
+export interface Page {
+  link: string;
+  view: React.FC;
+}
+
+const Landing = React.lazy(() => import("./Landing"));
+const Dashboard = React.lazy(() => import("./Dashboard"));
+const NotFound = React.lazy(() => import("./NotFound"));
 
 /**
  * Component
  */
 const App = (): JSX.Element => {
-  return <Landing />;
+  const pages: Page[] = [
+    {
+      link: "/",
+      view: Landing
+    },
+    {
+      link: "/dashboard",
+      view: Dashboard
+    }
+  ];
+
+  return (
+    <Router>
+      <React.Suspense fallback={Loader}>
+        <Switch>
+          {pages.map(page => (
+            <Route exact path={page.link} component={page.view} key={page.link} />
+          ))}
+          <Route path="*" component={NotFound} />
+        </Switch>
+      </React.Suspense>
+    </Router>
+  );
 };
 
 App.whyDidYouRender = true;
