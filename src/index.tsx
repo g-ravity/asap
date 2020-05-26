5a03eea3ebefa498276e20e59b8e126ce655d687
iamanishagarwal <iamanishagarwal@gmail.com>
2020-05-27 00:18:16 +0530

    feat: implement sentry for error logging in client and server

--- src/index.tsx
+++ src/index.tsx
@@ -1,9 +1,20 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import * as Sentry from "@sentry/browser";
 import App from "./components/App";
 import "./index.css";
 import * as serviceWorker from "./serviceWorker";
 import "./wdyr";
+
+if (process.env.REACT_APP_NODE_ENV === "production") {
+  Sentry.init({
+    dsn: process.env.REACT_APP_SENTRY_DSN,
+    attachStacktrace: true,
+    debug: true,
+    release: process.env.REACT_APP_RELEASE,
+    environment: process.env.REACT_APP_NODE_ENV
+  });
+}
 
 ReactDOM.render(
   <React.StrictMode>
