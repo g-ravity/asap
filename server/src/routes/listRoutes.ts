d792c4a9c6a9c5bb0d55d41803469b6139a1afe2
Ravik Ganguly <theravikganguly@gmail.com>
2020-06-01 20:37:14 +0530

    feat: registered Activity collection updates in listRoutes

--- server/src/routes/listRoutes.ts
+++ server/src/routes/listRoutes.ts
@@ -1,11 +1,12 @@
 import * as Sentry from "@sentry/node";
 import { Request, Response, Router } from "express";
 import { firestore } from "firebase-admin";
+import isEqual from "lodash/isEqual";
 import * as Yup from "yup";
-import { ItemIds, List, UserIdWithName } from "../../../types";
+import { Activity, ItemIds, List, Message, Project, UserIdWithName } from "../../../types";
 import { db } from "../config/firebase";
 import getSchema from "../config/yup";
-import { ListDBRef, ListDocRef, ProjectDocRef } from "../utils/firebaseContants";
+import { ActivityDBRef, ListDBRef, ListDocRef, ProjectDocRef } from "../utils/firebaseContants";
 
 export const router = Router({ mergeParams: true });
 
@@ -13,10 +14,10 @@ export const router = Router({ mergeParams: true });
  * Types
  */
 type AddListReq = Request<Pick<ItemIds, "projectId">, null, Pick<List, "name">>;
-type ListRes = Response<string>;
+type ListRes = Response<Message>;
 
 /**
- * Handler
+ * Create List Handler
  */
 router.post(
   "/",
@@ -27,11 +28,17 @@ router.post(
 
     try {
       const projectDoc = await ProjectDocRef(projectId).get();
+
+      // Permission Checks
       if (!projectDoc.exists) {
-        return res.status(404).send("Project doesn't exist!");
+        return res.status(404).send({ message: "Project doesn't exist!" });
+      }
+      if (!(projectDoc.data() as Project).members.filter(member => isEqual(member, req.user!)).length) {
+        return res.status(403).send({ message: "Not a member of the project" });
       }
 
       await ListSchema.validate(list);
+
       const user: UserIdWithName = { id: req.user!.id, name: req.user!.name };
       const listForSubmit: List = {
         ...list,
@@ -40,19 +47,31 @@ router.post(
         taskIds: []
       };
 
+      const activityForSubmit: Activity = {
+        activityType: "create",
+        activityItem: "list",
+        activityTime: listForSubmit.updatedAt,
+        activityCreator: user,
+        projectId,
+        newLabel: listForSubmit.name
+      };
+
       console.log("List For Submit: ", listForSubmit);
+      console.log("Activity For Submit: ", activityForSubmit);
 
       // Batched Write
       const batch = db.batch();
       const listDocRef = ListDBRef().doc();
       batch.set(listDocRef, listForSubmit);
+      const activityDocRef = ActivityDBRef().doc();
+      batch.set(activityDocRef, activityForSubmit);
       batch.update(ProjectDocRef(projectId), { listIds: firestore.FieldValue.arrayUnion(listDocRef.id) });
       await batch.commit();
 
-      return res.status(200).send("List created successfully!");
+      return res.status(200).send({ message: "List created successfully!" });
     } catch (err) {
       Sentry.captureException(err);
-      return res.status(500).send("Something Went Wrong!");
+      return res.status(500).send({ message: "Something Went Wrong!" });
     }
   }
 );
@@ -63,21 +82,31 @@ router.post(
 type UpdateListReq = Request<Pick<ItemIds, "projectId" | "listId">, null, Pick<List, "name">>;
 
 /**
- * Handler
+ * Update List Handler
  */
 router.put(
   "/:listId",
   async (req: UpdateListReq, res: ListRes): Promise<ListRes> => {
     const list = req.body;
-    const { listId } = req.params;
+    const { listId, projectId } = req.params;
     console.log("List ID: ", listId);
 
     try {
-      await ListSchema.validate(list);
-      const listDoc = await ListDocRef(listId).get();
-      if (!listDoc.exists) {
-        return res.status(404).send("List doesn't exist");
+      const projectDoc = await ProjectDocRef(projectId).get();
+
+      // Permission Checks
+      if (!projectDoc.exists) {
+        return res.status(404).send({ message: "Project doesn't exist!" });
       }
+      if (!(projectDoc.data() as Project).members.filter(member => isEqual(member, req.user!)).length) {
+        return res.status(403).send({ message: "Not a member of the project" });
+      }
+      if (!(projectDoc.data() as Project).listIds.includes(listId)) {
+        return res.status(404).send({ message: "List not found in Project" });
+      }
+
+      const listDoc = await ListDocRef(listId).get();
+      await ListSchema.validate(list);
 
       const user: UserIdWithName = { id: req.user!.id, name: req.user!.name };
       const listForSubmit: Partial<List> = {
@@ -86,13 +115,30 @@ router.put(
         updatedBy: user
       };
 
-      console.log("List For Submit: ", listForSubmit);
-      await ListDocRef(listId).update(listForSubmit);
+      const activityForSubmit: Activity = {
+        activityType: "update",
+        activityItem: "list",
+        activityTime: listForSubmit.updatedAt!,
+        activityCreator: user,
+        projectId,
+        oldLabel: (listDoc.data() as List).name,
+        newLabel: listForSubmit.name
+      };
 
-      return res.status(200).send("List successfully updated!");
+      console.log("List For Submit: ", listForSubmit);
+      console.log("Activity For Submit: ", activityForSubmit);
+
+      // Batch Write
+      const batch = db.batch();
+      batch.update(ListDocRef(listId), listForSubmit);
+      const activityDocRef = ActivityDBRef().doc();
+      batch.set(activityDocRef, activityForSubmit);
+      await batch.commit();
+
+      return res.status(200).send({ message: "List successfully updated!" });
     } catch (err) {
       Sentry.captureException(err);
-      return res.status(500).send("Something Went Wrong!");
+      return res.status(500).send({ message: "Something Went Wrong!" });
     }
   }
 );
@@ -103,7 +149,7 @@ router.put(
 type DeleteListReq = Request<Pick<ItemIds, "projectId" | "listId">, null, {}>;
 
 /**
- * Handler
+ * Delete List Handler
  */
 router.delete(
   "/:listId",
@@ -112,21 +158,44 @@ router.delete(
     console.log("List ID: ", listId);
 
     try {
-      const listDoc = await ListDocRef(listId).get();
-      if (!listDoc.exists) {
-        return res.status(404).send("List doesn't exist");
+      const projectDoc = await ProjectDocRef(projectId).get();
+
+      // Permission Checks
+      if (!projectDoc.exists) {
+        return res.status(404).send({ message: "Project doesn't exist!" });
       }
+      if (!(projectDoc.data() as Project).members.filter(member => isEqual(member, req.user!)).length) {
+        return res.status(403).send({ message: "Not a member of the project" });
+      }
+      if (!(projectDoc.data() as Project).listIds.includes(listId)) {
+        return res.status(404).send({ message: "List not found in Project" });
+      }
+
+      const listDoc = await ListDocRef(listId).get();
+
+      const activityForSubmit: Activity = {
+        activityType: "delete",
+        activityItem: "list",
+        activityTime: new Date(),
+        activityCreator: req.user!,
+        projectId,
+        oldLabel: (listDoc.data() as List).name
+      };
+
+      console.log("Activity For Submit: ", activityForSubmit);
 
       // Batched Write
       const batch = db.batch();
       batch.delete(ListDocRef(listId));
+      const activityDocRef = ActivityDBRef().doc();
+      batch.set(activityDocRef, activityForSubmit);
       batch.update(ProjectDocRef(projectId), { listIds: firestore.FieldValue.arrayRemove(listDoc.id) });
       await batch.commit();
 
-      return res.status(200).send("List successfully deleted!");
+      return res.status(200).send({ message: "List successfully deleted!" });
     } catch (err) {
       Sentry.captureException(err);
-      return res.status(500).send("Something Went Wrong!");
+      return res.status(500).send({ message: "Something Went Wrong!" });
     }
   }
 );
